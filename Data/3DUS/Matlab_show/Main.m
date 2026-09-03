b328b938ea8c89d3a2eda37afeccf2ccb43d0062
Tecnuc <paolo.tecchio95@gmail.com>
2026-09-03 11:14:08 +0200

    Added validation volumes outputs for inspection and playing around Quick edit the main to plug and play with the 3D data (validation volumes are slighlty different formatted)

--- Data/3DUS/Matlab_show/Main.m
+++ Data/3DUS/Matlab_show/Main.m
@@ -8,29 +8,81 @@ tmp = mfilename("fullpath");
 tmp = erase(tmp,mfilename);
 addpath(genpath(tmp))
 %pick up the mat file
-uiload();
+%uiload();
 
-%% Create GRIDs of transformed volume for plotting purpose
+[fileName, filePath] = uigetfile('*.mat', 'Select elaborated file');
 
-% Retrieve the world limits from RB
-xWorldLimits = RB.XWorldLimits;
-yWorldLimits = RB.YWorldLimits;
-zWorldLimits = RB.ZWorldLimits;
-% Define the number of points in each dimension
-nX = size(MaskedVol, 2);
-nY = size(MaskedVol, 1);
-nZ = size(MaskedVol, 3);
+%Check if user clicked Cancel (returns 0)
+if fileName == 0
+    return;
+else
+    %
+    fullFileName = fullfile(filePath, fileName);
 
-% Create meshgrid using the world limits and the number of points for
-% image volume
-[Xf, Yf, Zf] = meshgrid(linspace(xWorldLimits(1), xWorldLimits(2), nX), ...
-    linspace(yWorldLimits(1), yWorldLimits(2), nY), ...
-    linspace(zWorldLimits(1), zWorldLimits(2), nZ));
+    % Load the data into a structure
+    load(fullFileName);
 
-clearvars       xWorldLimits yWorldLimits zWorldLimits nX nY nZ
+    % fileName variable is now available for use
+    disp(['Loaded file: ', fileName]);
+end
 
-%FYI:
-%Cmap arranged is the colormap based on the width of aponerusis along its
-%main axis. i.e. red = wider, blue = narrower
 %% generate the volume slicer using the function I made
-new_plot_3d_volume_with_sliders(MaskedVol, Xf, Yf, Zf, fibers, 'file', Apo, TA, Cmap_arranged, {X,Y,Z,U,V,W}, T_landmark)
+
+
+if contains(fileName,"Synthetic","IgnoreCase",true)
+
+    %synthetic fascicle volume
+    % Retrieve the world limits from RB
+    xWorldLimits = RB.XWorldLimits;
+    yWorldLimits = RB.YWorldLimits;
+    zWorldLimits = RB.ZWorldLimits;
+    % Define the number of points in each dimension
+    nX = size(fas_vol, 2);
+    nY = size(fas_vol, 1);
+    nZ = size(fas_vol, 3);
+
+    % Create meshgrid using the world limits and the number of points for
+    % image volume
+    [Xf, Yf, Zf] = meshgrid(linspace(xWorldLimits(1), xWorldLimits(2), nX), ...
+        linspace(yWorldLimits(1), yWorldLimits(2), nY), ...
+        linspace(zWorldLimits(1), zWorldLimits(2), nZ));
+
+    clearvars       xWorldLimits yWorldLimits zWorldLimits nX nY nZ
+
+    %the fas_mesh is plot as "TA mesh" in the GUI
+    new_plot_3d_volume_with_sliders(fas_vol, Xf, Yf, Zf, fibers, 'file', Fake_apo_mesh, Fas_mesh, [0 1 0], {X,Y,Z,U,V,W}, Landmarks)
+
+else
+
+    %% Create GRIDs of transformed volume for plotting purpose
+
+    % Retrieve the world limits from RB
+    xWorldLimits = RB.XWorldLimits;
+    yWorldLimits = RB.YWorldLimits;
+    zWorldLimits = RB.ZWorldLimits;
+    % Define the number of points in each dimension
+    nX = size(MaskedVol, 2);
+    nY = size(MaskedVol, 1);
+    nZ = size(MaskedVol, 3);
+
+    % Create meshgrid using the world limits and the number of points for
+    % image volume
+    [Xf, Yf, Zf] = meshgrid(linspace(xWorldLimits(1), xWorldLimits(2), nX), ...
+        linspace(yWorldLimits(1), yWorldLimits(2), nY), ...
+        linspace(zWorldLimits(1), zWorldLimits(2), nZ));
+
+    clearvars       xWorldLimits yWorldLimits zWorldLimits nX nY nZ
+
+    if contains(fileName,"Real_fake","IgnoreCase",true)
+        %use the following function if you want to see the Phantom wire fascicle
+        %volume (this was the 3x3x3 mm^3 voxel size)
+        new_plot_3d_volume_with_sliders(MaskedVol, Xf, Yf, Zf, fibers, 'file', Apo, [], [0 1 0], {X,Y,Z,U,V,W}, Landmark)
+    else%//default case is a TA or whatever output of the script
+
+        %FYI:
+        %Cmap arranged is the colormap based on the width of aponerusis along its
+        %main axis. i.e. red = wider, blue = narrower
+        new_plot_3d_volume_with_sliders(MaskedVol, Xf, Yf, Zf, fibers, 'file', Apo, TA, Cmap_arranged, {X,Y,Z,U,V,W}, T_landmark)
+    end
+
+end
